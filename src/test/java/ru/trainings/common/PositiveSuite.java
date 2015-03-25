42fd70f5496876fcbce15fd0945969469453fe17
Bassman1988 <89032735516@yandex.ru>
2015-03-25 09:57:07 +0300

    поправил баг с запуском PositiveSuite

--- src/test/java/ru/trainings/common/PositiveSuite.java
+++ src/test/java/ru/trainings/common/PositiveSuite.java
@@ -4,10 +4,11 @@ import org.junit.experimental.categories.Categories;
 import org.junit.experimental.categories.Categories.IncludeCategory;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
+import ru.trainings.tests.TestClass;
 
 
 @SuiteClasses({
-	  Categories.class
+	 TestClass.class
 	})
 	@RunWith(Categories.class)
 	@IncludeCategory(MyCategories.PositiveTests.class)
