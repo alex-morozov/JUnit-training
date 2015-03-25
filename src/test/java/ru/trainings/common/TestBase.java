0574505fbea9ae7daebec45dfda7f975aa50a830
Bassman1988 <89032735516@yandex.ru>
2015-03-25 11:40:56 +0300

    8 задание. Переделал имеющиеся фикстуры, чтобы вместо @Before и @After использовались правила (@Rule)

--- src/test/java/ru/trainings/common/TestBase.java
+++ src/test/java/ru/trainings/common/TestBase.java
@@ -3,6 +3,9 @@ package ru.trainings.common;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.rules.ExternalResource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -11,14 +14,23 @@ public class TestBase {
 	public static WebDriver driver = new FirefoxDriver();
 	public static SeleniumAssertion check = new SeleniumAssertion (driver);		
 	public static String baseURL = "http://www.musicforums.ru/";
-		
-	@Before
-	  public void init (){
+	
+	@Rule
+	 public ExternalResource startDriverRule = new ExternalResource() {	
+		@Override
+		protected void before (){
 	  driver.get(baseURL);
 	  }
+	};
 	
-	@AfterClass
-	public static void stop (){
-		driver.quit();
-	}
+	
+	@ClassRule
+	 public static ExternalResource finishDriverRule = new ExternalResource() {	
+		@Override
+		protected void after (){
+			if (driver != null) {
+		        driver.quit();
+		      }
+	  }
+	};
 }
